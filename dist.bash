cc4763ec83fcf2f5b9e91d70a4a3fd8bd3647314
Raymond Reggers <raymond@adaptiv.nl>
2017-07-22 05:21:55 +0200

    hide output in order to work with travis

--- dist.bash
+++ dist.bash
@@ -3,11 +3,12 @@
 source version
 
 # sdist odoo
-ODOO_VERSION=$ODOO_VERSION ODOO_URL=$ODOO_URL python setup.py sdist
+echo "Running sdist for Odoo"
+ODOO_VERSION=$ODOO_VERSION ODOO_URL=$ODOO_URL python setup.py sdist &> /dev/null
 
 # sdist features
 features=($(ODOO_VERSION=$ODOO_VERSION python setup.py features))
 for f in "${features[@]:2}"; do
 echo "Running sdist for feature $f"
-ODOO_VERSION=$ODOO_VERSION FEATURE=$f python setup.py sdist
+ODOO_VERSION=$ODOO_VERSION FEATURE=$f python setup.py sdist &> /dev/null
 done
--- install.bash
+++ install.bash
@@ -3,11 +3,12 @@
 source version
 
 # install odoo
-pip install ./dist/odooku-odoo-$ODOO_VERSION.tar.gz
+echo "Installing Odoo"
+pip install ./dist/odooku-odoo-$ODOO_VERSION.tar.gz &> /dev/null
 
 # install features
 features=($(ODOO_VERSION=$ODOO_VERSION python setup.py features))
 for f in "${features[@]:2}"; do
-echo "Running sdist for feature $f"
-pip install ./dist/odooku-odoo-$f-$ODOO_VERSION.tar.gz
+echo "Installing feature $f"
+pip install ./dist/odooku-odoo-$f-$ODOO_VERSION.tar.gz &> /dev/null
 done
